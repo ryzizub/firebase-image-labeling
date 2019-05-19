e48c31ac5c6ba9f58d237f8b3b22e37bd266f943
ryzizub <dominik.simonik@gmail.com>
2019-05-19 21:53:03 +0200

    init label detection

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,12 +2,36 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp(functions.config().firebase);
 
+// tslint:disable-next-line: no-implicit-dependencies
+import path = require('path');
+import os = require('os');
+
+const vision = require('@google-cloud/vision');
+
+
 export const newCity = functions.storage
     .object()
     .onFinalize((object) => {
-        return admin.firestore().collection('images').add({
-            fileName: object.name,
-            tags: []
-        })
+
+        const fileBucket = object.bucket;
+        const filePath = object.name;
+
+        return getLabelsForImage(fileBucket, filePath)
+            .then((tags) => {
+                return admin.firestore().collection('images').add({
+                    fileName: filePath,
+                    tags: tags
+                });
+            });
     });
-    
+
+async function getLabelsForImage(fileBucket: string, fileName: string = 'something'): Promise<string[]> {
+    const bucket = admin.storage().bucket(fileBucket);
+    const tempFilePath = path.join(os.tmpdir(), fileName);
+    await bucket.file(fileName).download({destination: tempFilePath});
+
+    const client = new vision.ImageAnnotatorClient();
+    const [result] = await client.labelDetection(tempFilePath);
+    const labels = result.labelAnnotations;
+    return labels.map((label: { description: null; }) => label.description);
+}
